b8e30a1e32e0e093818d9367698ee2394483f03e
Aidmora <arielabc389@gmail.com>
2025-04-24 23:28:49 -0500

    Primmer commit

---
+++ public/script.js
@@ -0,0 +1,5 @@
+// Función para mostrar el mensaje cuando se hace clic en el botón
+document.getElementById("proposeButton").addEventListener("click", function() {
+    // Mostrar el mensaje emergente
+    document.getElementById("messagePopup").style.display = "block";
+});
